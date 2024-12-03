4445eff4662ff9c2f98e683a5dfde09edb19b84d
Evgenia-Fro <ilyasova493@gmail.com>
2024-12-04 00:26:49 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as r_1
+import true_math as r_2
+
+result1 = r_1.divide(69, 3)
+result2 = r_1.divide(3, 0)
+result3 = r_2.divide(49, 7)
+result4 = r_2.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
